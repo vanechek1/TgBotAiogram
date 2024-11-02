078586890317ce3106ef4c05fd355843c9ebe0e4
vanechek1 <ivankicherov@mail.ru>
2024-11-02 13:33:19 +0500

    inline buttons

--- main.py
+++ main.py
@@ -35,14 +35,24 @@ def create_connection():
     except Error as e: print(f"The error '{e}' occured")
     return connection
 
-def execute_query(connection, query):
+# def execute_query(connection, query):
+#     with connection.cursor() as cursor:
+#         try:
+#             cursor.execute(query)
+#             result = cursor.fetchall()
+#             for i in range(len(result)):
+#                 return result[0][i]
+#         except Error as e: print(f"The error '{e}' occured")
+
+def execute_query(connection, query, params=None):
     with connection.cursor() as cursor:
         try:
             cursor.execute(query)
-            result = cursor.fetchall()
-            for i in range(len(result)):
-                return result[0][i]
-        except Error as e: print(f"The error '{e}' occured")
+            result = cursor.fetchall()  # Получаем все записи
+            return result  # Возвращаем все записи
+        except Error as e:
+            print(f"The error '{e}' occurred")
+            return []  # Возвращаем пустой список в случае ошибки
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -80,15 +90,28 @@ async def question(message: types.Message):
 @dp.message_handler(content_types=['text'])
 async def askquestion(message: types.Message):
     if 'educon' in message.text:
-        await message.reply(execute_query(create_connection(),"SELECT answer FROM information WHERE question LIKE '%educon%'"))
+        ans = execute_query(create_connection(), "SELECT * FROM information WHERE answer LIKE '%educon%'")
+        if ans:
+            markup = types.InlineKeyboardMarkup()  # количество кнопок в ряду
+            for record in ans:
+                question_id, question, answer = record
+                markup.add(types.InlineKeyboardButton(f'{question}', callback_data=f'question_{question_id}'))
+            await message.reply('Выберите вопрос из списка:', reply_markup=markup)
+        else:
+            response = "Записей с 'educon' не найдено."
+            await message.reply(response)
+        # await message.reply(execute_query(create_connection(), "SELECT * FROM information WHERE answer LIKE '%educon%'"))
+        # await message.reply(execute_query(create_connection(),"SELECT answer FROM information WHERE question LIKE '%educon%'"))
     elif 'арми' in message.text:
         await message.reply(execute_query(create_connection(),"SELECT answer FROM information WHERE question LIKE '%арми%'"))
     else:
         await message.reply('Я не понимаю, чего вы хотите')
 
-    # await message.reply('aa')
-
-
+@dp.callback_query_handler(lambda c: c.data.startswith('question_'))
+async def process_question(callback_query: types.CallbackQuery):
+    question_id = str(callback_query.data.split('_')[1])
+    ans = execute_query(create_connection(), "SELECT answer FROM information WHERE id = "+str(question_id))
+    await callback_query.message.answer(ans[0][0])
 
 @dp.callback_query_handler(lambda c: c.data in ['q1','q2','q3'])
 async def popularQuestions(call: types.callback_query):
